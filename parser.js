f100b2b161b6e299e2dff6f19fdde2058d5ae367
Syed Abdullah Ali <mtuais95@gmail.com>
2020-09-18 15:05:48 +0400

    first commit

---
+++ parser.js
@@ -0,0 +1,8 @@
+const nightmare = require('nightmare')()
+
+"priceblock_ourprice"
+
+
+async function checkPrice() {
+    nightmare.goto("https://www.amazon.com/Western-Digital-Elements-Portable-External/dp/B06W55K9N6")
+}
